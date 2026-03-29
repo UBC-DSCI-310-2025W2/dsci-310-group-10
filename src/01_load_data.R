ebf024594ff3de572e7170bfbed7511447e1b861
dty200 <1550769610@qq.com>
2026-03-28 21:12:02 -0700

    add load_data function and tests

--- src/01_load_data.R
+++ src/01_load_data.R
@@ -2,6 +2,9 @@ library(tidyverse)
 library(docopt)
 library(OpenML)
 
+
+source("R/load_data.R")
+
 doc <- "
 Usage:
   01_load_data.R <dataset_id> <output_file>
@@ -9,14 +12,9 @@ Usage:
 
 opt <- docopt(doc)
 
-dataset_id <- as.numeric(opt$dataset_id)
+dataset_id <- opt$dataset_id
 output_file <- opt$output_file
 
-# Download dataset from OpenML
-oml_data <- getOMLDataSet(data.id = dataset_id)
+df <- load_data(dataset_id)
 
-# Extract dataframe
-df <- oml_data$data
-
-# Save dataset
 saveRDS(df, output_file)
---
+++ tests/testthat/test_load_data.R
@@ -0,0 +1,35 @@
+library(testthat)
+library(caret)
+library(here)
+source(here("R", "load_data.R"))
+
+
+test_that("load_data returns a data frame", {
+  
+  df <- load_data(61)  # iris dataset in OpenML
+  
+  expect_true(is.data.frame(df))
+  expect_gt(nrow(df), 0)
+  expect_gt(ncol(df), 0)
+})
+
+
+test_that("load_data returns consistent structure", {
+  
+  df <- load_data(61)
+  
+  expect_named(df)
+})
+
+test_that("load_data fails on invalid input", {
+  
+  expect_error(load_data(NA))
+  expect_error(load_data(NULL))
+})
+
+test_that("load_data fails on invalid input", {
+  
+  expect_error(load_data(NA))
+  expect_error(load_data(NULL))
+})
+
